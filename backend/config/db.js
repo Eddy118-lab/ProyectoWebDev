172781fc0d2227003d9c6b59c109aec4fa4f01f0
Eddy118-lab <erodrigueze4@miumg.edu.gt>
2024-11-05 20:11:05 -0600

    UPDATE

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
         });
-        console.log('Conexión a la base de datos MongoDB establecida correctamente.');
+        console.log('Conexión a la base de datos MongoDB Atlas establecida correctamente.');
     } catch (error) {
         console.error('Error al conectar a la base de datos:', error.message);
         process.exit(1); // Terminar el proceso en caso de error
